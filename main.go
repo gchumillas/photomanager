ca8cc1ae361d8782864c88c29a412429de1f5897
Gonzalo Chumillas <gonzalo.chumillas@gmail.com>
2019-01-13 22:09:15 +0100

    recode

--- main.go
+++ main.go
@@ -13,5 +13,9 @@ func main() {
 	fmt.Println("Starting the server...")
 	r := mux.NewRouter()
 	r.HandleFunc("/home", handler.GetHome).Methods("GET")
-	log.Fatal(http.ListenAndServe(":8080", r))
+
+	done := make(chan bool)
+	go http.ListenAndServe(":8080", r)
+	log.Printf("Server started at port %v", "8080")
+	<-done
 }
